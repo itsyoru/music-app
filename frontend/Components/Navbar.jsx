a4024c4ff5477aedf6ff9407dd5bc6dbabb47ab8
kne27 <163606760+kne27@users.noreply.github.com>
2024-04-30 18:17:05 -0400

    Add files via upload

--- frontend/Components/Navbar.jsx
+++ frontend/Components/Navbar.jsx
@@ -6,23 +6,23 @@ const Navbar = () => {
     return (
         <nav style={{ position: 'fixed', top: 0, left: 0, right: 0 }}>
            <Link to="/" style={{ position: 'absolute', left: '70px', top: '22px', fontSize: '20px', color: '#616336' }}>
-                DEN
+                D E N
             </Link>
             <ul style={{ display: 'flex', listStyle: 'none', justifyContent: 'center', paddingTop: '10px' }}>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="/login" style={{ color: '#616336', fontSize: '20px' }}>Login</Link>
+                <li style={{ marginRight: '13px' }}>
+                    <Link to="/login" style={{ color: '#616336', fontSize: '12px' }}>L O G I N</Link>
                 </li>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="/register" style={{ color: '#616336', fontSize: '20px' }}>Register</Link>
+                <li style={{ marginRight: '15px' }}>
+                    <Link to="/register" style={{ color: '#616336', fontSize: '12px' }}>R E G I S T E R</Link>
                 </li>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="/profile" style={{ color: '#616336', fontSize: '20px' }}>Profile</Link>
+                <li style={{ marginRight: '16px' }}>
+                    <Link to="/profile" style={{ color: '#616336', fontSize: '12px' }}>P R O F I L E</Link>
                 </li>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="/browse" style={{ color: '#616336', fontSize: '20px' }}>Browse</Link>
+                <li style={{ marginRight: '17px' }}>
+                    <Link to="/browse" style={{ color: '#616336', fontSize: '12px' }}>B R O W S E R</Link>
                 </li>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="/createaparty" style={{ color: '#616336', fontSize: '20px' }}>Parties</Link>
+                <li style={{ marginRight: '18px' }}>
+                    <Link to="/createaparty" style={{ color: '#616336', fontSize: '12px' }}>P A R T I E S</Link>
                 </li>
             </ul>
         </nav>
